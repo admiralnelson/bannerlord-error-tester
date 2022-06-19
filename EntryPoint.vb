21829d6837befd1741acfb8c77e279f9d5716269
admiralnelson <admiralofinternetmeme@outlook.com>
2022-06-19 19:36:03 +0700

    first commit

---
+++ EntryPoint.vb
@@ -0,0 +1,23 @@
+﻿Imports TaleWorlds
+Imports TaleWorlds.InputSystem
+
+Namespace Global.ErrorTester
+    Public Class SubModuleEntryPoint
+        Inherits MountAndBlade.MBSubModuleBase
+
+        Protected Overrides Sub OnSubModuleLoad()
+            MyBase.OnSubModuleLoad()
+            InputMonitor.RegisterInputKey(InputKey.Home, Sub()
+                                                             Console.WriteLine("test")
+                                                         End Sub)
+            InputMonitor.RegisterInputKey(InputKey.Insert, Sub()
+                                                               Throw New Exception("exception test!")
+                                                           End Sub)
+        End Sub
+
+        Protected Overrides Sub OnApplicationTick(dt As Single)
+            MyBase.OnApplicationTick(dt)
+            InputMonitor.Tick()
+        End Sub
+    End Class
+End Namespace
---
+++ InputMonitor.vb
@@ -0,0 +1,27 @@
+﻿Imports TaleWorlds.InputSystem
+Imports TaleWorlds.Core
+
+Public Class InputMonitor
+
+    Shared keyValue As Dictionary(Of InputKey, List(Of Action))
+    Public Shared Sub Tick()
+        For Each key In keyValue
+            If Input.IsReleased(key.Key) Then
+                For Each act In key.Value
+                    act()
+                Next
+            End If
+        Next
+    End Sub
+
+    Public Shared Sub RegisterInputKey(key As InputKey, callback As Action)
+        If keyValue Is Nothing Then keyValue = New Dictionary(Of InputKey, List(Of Action))
+        If keyValue.ContainsKey(key) Then
+            keyValue(key).Add(callback)
+            Exit Sub
+        End If
+        keyValue.Add(key, New List(Of Action))
+        keyValue(key).Add(callback)
+    End Sub
+
+End Class
